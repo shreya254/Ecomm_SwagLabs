8f4b5180a997160a3fc90d18e0c5015b1a9be664
shreya254 <shreyasshsn.1@gmail.com>
2025-07-26 01:37:54 +0100

    change in feature branch

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,7 +4,7 @@ describe('login to the application', ()=>{
         cy.get('input[data-test="username"]').type('standard_user')
         cy.get('input[data-test="password"]').type('secret_sauce')
         cy.get('[data-test="login-button"]').click()
-       cy.url().should('contains', 'inventory.html')
+       cy.urls().should('contains', 'inventory.html')
         
 
     })
