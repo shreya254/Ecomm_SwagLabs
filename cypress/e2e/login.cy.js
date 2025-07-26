f613f150bca74af5fe44d2f30dfbd5738bba415f
shreya254 <shreyasshsn.1@gmail.com>
2025-07-26 01:06:53 +0100

    changes in login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,9 @@
 describe('login to the application', ()=>{
     it('login',()=>{
-        cypress.visit('https://www.saucedemo.com/inventory.html')
+        cy.visit('https://www.saucedemo.com')
+        cy.get('input[data-test="username"]').type('standard_user')
+        cy.get('input[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+
     })
 })
