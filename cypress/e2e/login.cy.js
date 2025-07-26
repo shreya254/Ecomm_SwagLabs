05748f2fe6789559cc00f07eba72852513b22e26
shreya254 <shreyasshsn.1@gmail.com>
2025-07-26 01:22:52 +0100

    added assertion

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,6 +4,8 @@ describe('login to the application', ()=>{
         cy.get('input[data-test="username"]').type('standard_user')
         cy.get('input[data-test="password"]').type('secret_sauce')
         cy.get('[data-test="login-button"]').click()
+       cy.url().should('contains', 'inventory.html')
+        
 
     })
 })
